edb9d037ddd183032763d4e59440484e774d8457
Evan <evanacoates@gmail.com>
2023-04-10 14:31:34 -0400

    query selectors for temp and city

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,14 @@
 const apiKey = "ecdebc01d90a5c1e75556e5c58758333";
-const apiUrl ="https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+const apiUrl ="https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={ecdebc01d90a5c1e75556e5c58758333}";
 
 async function pullWeather(){
     const response = await fetch(apiUrl + '&appid=${apiKey}')
-    var data = await response.json();
+    var forecast = await response.json();
 
-    console.log(data)
+    console.log(forecast)
+
+    document.querySelector(".city").innerHTML =forecast.name;
+    document.querySelector(".temp").innerHTML =forecast.main.temp;
 }
 pullWeather();
 
