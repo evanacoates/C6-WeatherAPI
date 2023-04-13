0e11ef909e59c9b23ca0289a78cf51f19ef47db9
Evan <evanacoates@gmail.com>
2023-04-13 11:21:43 -0400

    api key changes, variable changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,9 @@
 const apiKey = "ecdebc01d90a5c1e75556e5c58758333";
-const apiUrl ="https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={ecdebc01d90a5c1e75556e5c58758333}";
+const apiUrl ="https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 
 async function pullWeather(){
     const response = await fetch(apiUrl + '&appid=${apiKey}')
-    var forecast = await response.json();
+    let forecast = await response.json();
 
     console.log(forecast)
 
